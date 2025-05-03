012e1deaa207f0f0dd1f5eec5ef32a2bf7d382dd
Sam Manibog <sfmanibog@outlook.com>
2025-05-02 22:11:30 -0400

    added package installation functionality

---
+++ install.sh
@@ -0,0 +1,42 @@
+#install packages
+packages=(
+    #hyprland DE
+    hyprland
+    hyprpaper
+    hyprpicker
+    hypridle
+    hyprlock
+    hyprpolkitagent
+    hyprsysteminfo
+    sddm
+
+    #waybar
+    waybar
+    wireplumber
+    libgtop
+    bluez
+    bluez-utils
+    btop
+    networkmanager
+    wl-clipboard
+    wl-clip-persist
+    brightnessctl
+    upower
+    power-profiles-daemon
+    wf-recorder
+
+    #applications
+    neovim
+    kitty
+    firefox
+
+    #file manager
+    thunar
+    thunar-archive-plugin
+    thunar-media-tags-plugin
+    thunar-volman
+
+)
+
+pacman -Syuu
+pacman -S --needed $(packages)
